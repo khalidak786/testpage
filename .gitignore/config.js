af3c857bcab9c166221c101404efa90573384b46
khalidak786 <146335163+khalidak786@users.noreply.github.com>
2025-01-15 18:52:19 +0400

    Update config.js
    Signed-off-by: khalidak786 <146335163+khalidak786@users.noreply.github.com>

--- .gitignore/config.js
+++ .gitignore/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     username: 'wePFbGGPCofPrqOxUUiMcEWc',
-    password: 'your_password'
+    password: 'Z6tDh8rZeKRwbT-deyf+FRu8,75MD3q3GIFKinGkw-t22BE3D8uFqSLuPmboYOamyLUo++YZ_Bs-KbU2w25PZTIFAJ14CbszdvZeF8W-yU.EjdbgH8UzKcm.osM3_-EZ'
 };
