4dc713e8853ff897a4cad3158977903a12385478
khalidak786 <146335163+khalidak786@users.noreply.github.com>
2025-01-16 19:22:20 +0400

    Delete .gitignore directory
    Signed-off-by: khalidak786 <146335163+khalidak786@users.noreply.github.com>

--- .gitignore/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    username: 'wePFbGGPCofPrqOxUUiMcEWc',
-    password: 'Z6tDh8rZeKRwbT-deyf+FRu8,75MD3q3GIFKinGkw-t22BE3D8uFqSLuPmboYOamyLUo++YZ_Bs-KbU2w25PZTIFAJ14CbszdvZeF8W-yU.EjdbgH8UzKcm.osM3_-EZ'
-};
